f518454894a76891e712681256a2672a426a16e4
hor20009 <hor20009@byui.edu>
2022-09-24 21:50:28 -0500

    Fix a problem?

--- controllers/learn.js
+++ controllers/learn.js
@@ -114,7 +114,7 @@ exports.getUserProject = (req, res, next) => {
                     projectId: userProject._id,
                     projectCreated: niceDate(userProject.saveDate),
                     projectUpdated: niceDate(userProject.editDate),
-                    messages: await userProject.getMessages(req.user._id),
+                    messages: await userProject.getMessages(req.user?._id),
                     isOwn: (userProject.userId._id.toString() == req.user._id.toString()),
                     user: {
                         username: user.username,
